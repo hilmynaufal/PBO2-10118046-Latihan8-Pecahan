5d5ff2811a4a0410acb7e46f509dbba0a1eb07b5
ASUS <ASUS@DESKTOP-6TMOBF9>
2019-09-26 19:17:48 +0700

    commit

---
+++ src/pbo2/pert1/pkg10118046/latihan8/pecahan/PBO2PERT110118046Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pert1.pkg10118046.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan konsep pecahan pada variable dengan menggunakan tipe data float dan double
+ */
+public class PBO2PERT110118046Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+    }
+
+}
